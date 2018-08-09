ab30ac3e6341057e2f2c20b36d8d06ad3aed5e8a
thekayode <42251003+thekayode@users.noreply.github.com>
2018-08-09 19:12:59 +0000

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,7 @@
+name = input ('what is your name?:')
+print(name)
+age = int(input('how old are you?:'))
+print(age)
+c = 100 - age
+hundred = 2018 + c
+print('You will be hundred years old in', hundred)
